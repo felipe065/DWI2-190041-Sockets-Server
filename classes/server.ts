2b173bbcde23c3c7720b5d5b58931b37b4ec488c
FelipeParedes <felipeparedes238@gmail.com>
2022-06-22 14:34:30 -0500

    Segundo Commit: Implementacion del patron de diseño singleton en la clase server

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,39 @@
-import express from 'express';
-import { SERVER_PORT } from '../global/enviroment';
+import express from "express";
+import { SERVER_PORT } from "../global/enviroment";
+import socketIO from "socket.io";
+import http from "http";
 
 export default class Server {
-    public app: express.Application;
-    public port: number;
+  private static _instance: Server;
+  public app: express.Application;
+  public port: number;
 
-    constructor(){
-        this.app= express();
-        this.port= SERVER_PORT;
-    }
+  public io: socketIO.Server;
+  private httpServer: http.Server;
 
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
 
-    //start(callback: () => void){
-    start(callback: Function){
-        this.app.listen(this.port, callback());
-    }
+    this.httpServer = new http.Server(this.app);
+    this.io = new socketIO.Server(this.httpServer);
+
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log("Escuchando conexiones - sockets");
+    this.io.on("conection", (cliente) => {
+      console.log("Cliente conectado");
+    });
+  }
+
+  //start(callback: () => void){
+  start(callback: Function) {
+    this.httpServer.listen(this.port, callback());
+  }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { router } from "./routes/route";
 import bodyParser from "body-parser";
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 //Body Parser
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
